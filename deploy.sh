65739a92589e1658464967904f02a376b79d560b
Serio <SeriousNuts@yandex.ru>
2024-11-27 23:09:28 +0700

    add docker build46

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-install -m 600 -D /dev/null ~/.ssh/id_rsa
-echo "${{ secrets.SSH_PRIVATE_KEY }}" > ~/.ssh/id_rsa
-ssh-keyscan -H ${{ secrets.SSH_HOST }} > ~/.ssh/known_hosts
 docker login -u ${{ secrets.DOCKER_HUB_USERNAME }} -p ${{ secrets.DOCKER_HUB_PASS }}
 docker pull ${{ secrets.DOCKER_HUB_USERNAME }}/${{ vars.PROJECT_NAME }}:${{ github.ref_name }}-${{ github.run_number }}
 docker stop $(docker ps -aq)
