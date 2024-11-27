e24a37af6ade6ed7984b613cd2a373dc6672c4ad
Serio <SeriousNuts@yandex.ru>
2024-11-27 23:25:55 +0700

    add docker build49

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+echo ${{ github.ref_name }}-${{ github.run_number }}
 docker login -u ${{ secrets.DOCKER_HUB_USERNAME }} -p ${{ secrets.DOCKER_HUB_PASS }}
 docker pull ${{ secrets.DOCKER_HUB_USERNAME }}/${{ vars.PROJECT_NAME }}:${{ github.ref_name }}-${{ github.run_number }}
 docker stop $(docker ps -aq)
-docker system prune || true
 docker run -d -p 5000:5000 ${{ secrets.DOCKER_HUB_USERNAME }}/${{ vars.PROJECT_NAME }}:${{ github.ref_name }}-${{ github.run_number }}
